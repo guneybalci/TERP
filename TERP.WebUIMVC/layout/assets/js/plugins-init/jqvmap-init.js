e8abd5c913354990c29dddbf1446d7fd6fe69026
Güney BALCI <guneybalci92@hotmail.com>
2021-04-11 00:44:31 +0300

    homepage completed

--- TERP.WebUIMVC/layout/assets/js/plugins-init/jqvmap-init.js
+++ TERP.WebUIMVC/layout/assets/js/plugins-init/jqvmap-init.js
@@ -20,61 +20,65 @@
 			borderColor: 'rgb(239, 242, 244)',
 			borderOpacity: 0.25,
 			borderWidth: 1,
-			color: 'rgb(239, 242, 244)',
+			// color: 'rgb(239, 242, 244)',
+			color: 'rgb(101,103,115)',
 			enableZoom: true,
-			hoverColor: 'rgba(239, 242, 244 0.9)',
+			// hoverColor: 'rgba(239, 242, 244 0.9)',
+			hoverColor: 'rgba(59, 50, 137, 0.9)',
 			hoverOpacity: null,
 			normalizeFunction: 'linear',
 			scaleColors: ['#b6d6ff', '#005ace'],
-			selectedColor: 'rgba(239, 242, 244 0.9)',
+			// selectedColor: 'rgba(239, 242, 244 0.9)',
+			selectedColor: 'rgba(239,127,27,0.9)',
 			selectedRegions: null,
 			showTooltip: true,
 			onRegionClick: function(element, code, region)
 			{
-				var message = 'You clicked "'
+				var message = 'Ülke:'
 					+ region
-					+ '" which has the code: '
+					+ ', Uluslararası Kodu: '
 					+ code.toUpperCase();
 		 
-				alert(message);
+				// alert(message);
+				document.getElementById("region-name").innerHTML=message;
 			}
 		});
 	}
 	
-	var handleUsaMap = function(trigger = 'load'){
-		var vmapSelector = $('#usa');
-		if(trigger == 'resize')
-		{
-			vmapSelector.empty();
-			vmapSelector.removeAttr('style');
-		}
+	// var handleUsaMap = function(trigger = 'load'){
+	// 	var vmapSelector = $('#usa');
+	// 	if(trigger == 'resize')
+	// 	{
+	// 		vmapSelector.empty();
+	// 		vmapSelector.removeAttr('style');
+	// 	}
 		
-		vmapSelector.delay(500).unbind().vectorMap({ 
-			map: 'usa_en',
-			backgroundColor: 'transparent',
-			borderColor: 'rgb(239, 242, 244)',
-			borderOpacity: 0.25,
-			borderWidth: 1,
-			color: 'rgb(239, 242, 244)',
-			enableZoom: true,
-			hoverColor: 'rgba(239, 242, 244 0.9)',
-			hoverOpacity: null,
-			normalizeFunction: 'linear',
-			scaleColors: ['#b6d6ff', '#005ace'],
-			selectedColor: 'rgba(239, 242, 244 0.9)',
-			selectedRegions: null,
-			showTooltip: true,
-			onRegionClick: function(element, code, region)
-			{
-				var message = 'You clicked "'
-					+ region
-					+ '" which has the code: '
-					+ code.toUpperCase();
+	// 	vmapSelector.delay(500).unbind().vectorMap({ 
+	// 		map: 'usa_en',
+	// 		backgroundColor: 'transparent',
+	// 		borderColor: 'rgb(239, 242, 244)',
+	// 		borderOpacity: 0.25,
+	// 		borderWidth: 1,
+	// 		color: 'rgb(239, 242, 244)',
+	// 		enableZoom: true,
+	// 		hoverColor: 'rgba(239, 242, 244 0.9)',
+	// 		hoverOpacity: null,
+	// 		normalizeFunction: 'linear',
+	// 		scaleColors: ['#b6d6ff', '#005ace'],
+	// 		selectedColor: 'rgba(239, 242, 244 0.9)',
+	// 		selectedRegions: null,
+	// 		showTooltip: true,
+	// 		onRegionClick: function(element, code, region)
+	// 		{
+	// 			var message = 'You clicked "'
+	// 				+ region
+	// 				+ '" which has the code: '
+	// 				+ code.toUpperCase();
 		 
-				alert(message);
-			}
-		});
-	}
+	// 			alert(message);
+	// 		}
+	// 	});
+	// }
 	
 		return {
 			init:function(){
@@ -83,12 +87,12 @@
 			
 			load:function(){
 				handleWorldMap();
-				handleUsaMap();
+				// handleUsaMap();
 			},
 			
 			resize:function(){
 				handleWorldMap('resize');
-				handleUsaMap('resize');
+				// handleUsaMap('resize');
 			}
 		}
 	
