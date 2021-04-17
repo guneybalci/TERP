592de1bca9811702240f602b5f6cf0e5382721c2
Guney Balci <guneybalcibilgeadam@hotmail.com>
2021-04-17 18:16:22 +0300

    email part1-1

--- TERP.WebUIMVC/layout/assets/vendor/dropzone/dist/dropzone.js
+++ TERP.WebUIMVC/layout/assets/vendor/dropzone/dist/dropzone.js
@@ -440,56 +440,56 @@ function (_Emitter) {
         /**
          * The text used before any files are dropped.
          */
-        dictDefaultMessage: "Drop files here to upload",
+        dictDefaultMessage: "Dosyaları yüklemek için buraya bırakın",
 
         /**
          * The text that replaces the default message text it the browser is not supported.
          */
-        dictFallbackMessage: "Your browser does not support drag'n'drop file uploads.",
+        dictFallbackMessage: "Tarayıcınız sürükle-bırak dosya yüklemelerini deskteklemiyor",
 
         /**
          * The text that will be added before the fallback form.
          * If you provide a  fallback element yourself, or if this option is `null` this will
          * be ignored.
          */
-        dictFallbackText: "Please use the fallback form below to upload your files like in the olden days.",
+        dictFallbackText: "Dosyalarınızı eski günlerdeki gibi yüklemek için lütfen aşağıdaki geri dönüş formunu kullanın.",
 
         /**
          * If the filesize is too big.
          * `{{filesize}}` and `{{maxFilesize}}` will be replaced with the respective configuration values.
          */
-        dictFileTooBig: "File is too big ({{filesize}}MiB). Max filesize: {{maxFilesize}}MiB.",
+        dictFileTooBig: "Dosya çok büyük ({{dosyaboyutu}} MiB). Maks dosya boyutu: {{maxFilesize}} MiB.",
 
         /**
          * If the file doesn't match the file type.
          */
-        dictInvalidFileType: "You can't upload files of this type.",
+        dictInvalidFileType: "Bu türdeki dosyaları yükleyemezsiniz.",
 
         /**
          * If the server response was invalid.
          * `{{statusCode}}` will be replaced with the servers status code.
          */
-        dictResponseError: "Server responded with {{statusCode}} code.",
+        dictResponseError: "Sunucu, {{statusCode}} koduyla yanıt verdi.",
 
         /**
          * If `addRemoveLinks` is true, the text to be used for the cancel upload link.
          */
-        dictCancelUpload: "Cancel upload",
+        dictCancelUpload: "Yüklemeyi iptal et",
 
         /**
          * The text that is displayed if an upload was manually canceled
          */
-        dictUploadCanceled: "Upload canceled.",
+        dictUploadCanceled: "Yükleme iptal edildi.",
 
         /**
          * If `addRemoveLinks` is true, the text to be used for confirmation when cancelling upload.
          */
-        dictCancelUploadConfirmation: "Are you sure you want to cancel this upload?",
+        dictCancelUploadConfirmation: "Bu yüklemeyi iptal etmek istediğinizden emin misiniz?",
 
         /**
          * If `addRemoveLinks` is true, the text to be used to remove a file.
          */
-        dictRemoveFile: "Remove file",
+        dictRemoveFile: "Dosyayı kaldır",
 
         /**
          * If this is not null, then the user will be prompted before removing a file.
@@ -500,7 +500,7 @@ function (_Emitter) {
          * Displayed if `maxFiles` is st and exceeded.
          * The string `{{maxFiles}}` will be replaced by the configuration value.
          */
-        dictMaxFilesExceeded: "You can not upload any more files.",
+        dictMaxFilesExceeded: "Daha fazla dosya yükleyemezsiniz.",
 
         /**
          * Allows you to translate the different units. Starting with `tb` for terabytes and going down to
