6fcf2e0c7a71283b988c9c85ce7235c19f7cc8e9
Güney Balcı <guneybalci@gmail.com>
2021-04-24 15:17:24 +0300

    file-upload completed-3

--- TERP.WebUIMVC/Layout/assets/js/plugins-init/file-upload-init.js
+++ TERP.WebUIMVC/Layout/assets/js/plugins-init/file-upload-init.js
@@ -56,7 +56,7 @@ $(document).ready(function(){
    });
 
    $(".btn-new").on('click',function(){
-        $("#uploader").append('<div class="row uploadDoc"><div class="col-sm-3"><div class="docErr">Please upload valid file</div><!--error--><div class="fileUpload btn btn-orange"> <img src="../assets/images/fileupload/file-upload-empty.svg" class="icon"><span class="upl" id="upload">Dosya Ekle</span><input type="file" class="upload up" id="up" onchange="readURL(this);" /></div></div><div class="col-sm-9"><input type="text" class="form-control" name="" placeholder="Buradan notunuzu ekleyebilirsiniz.."><a class="btn-check"><i class="fa fa-times"></i> Dosyayı Sil</a></div></div>');
+        $("#uploader").append('<div class="row uploadDoc"><div class="col-sm-3"><div class="fileUpload btn btn-orange"> <img src="../assets/images/fileupload/file-upload-empty.svg" class="icon"><span class="upl" id="upload">Dosya Ekle</span><input type="file" class="upload up" id="up" onchange="readURL(this);" /></div></div><div class="col-sm-9">	<div class="docErr col-sm-12">Lütfen geçerli dosya yükleyin!</div><input type="text" class="form-control addNote" name="" placeholder="Buradan notunuzu ekleyebilirsiniz.."><a class="btn-check"><i class="fa fa-times"></i> Dosyayı Sil</a></div></div>');
    });
     
    $(document).on("click", "a.btn-check" , function() {
