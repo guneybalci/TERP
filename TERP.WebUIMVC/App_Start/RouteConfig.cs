6961879a7d9d6c4f74a6e6bdcd777d3341dd9471
Abdullah ZENGİN <abdullah-zengin@hotmail.com.tr>
2021-05-20 22:51:00 +0300

    add mvc ui layer then implemented design from template

--- TERP.WebUIMVC/App_Start/RouteConfig.cs
+++ TERP.WebUIMVC/App_Start/RouteConfig.cs
@@ -14,6 +14,42 @@ namespace TERP.WebUIMVC
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Homepage",
+                url: "anasayfa",
+                defaults: new { controller = "Home", action = "Index" }
+            );
+
+            routes.MapRoute(
+                name: "CarControl",
+                url: "arac-yonetim",
+                defaults: new { controller = "Car", action = "Index" }
+            );
+
+            routes.MapRoute(
+                name: "PersonManagement",
+                url: "personel-yonetim",
+                defaults: new { controller = "Personal", action = "Index" }
+            );
+
+            routes.MapRoute(
+                name: "CostManagement",
+                url: "masraf-yonetim",
+                defaults: new { controller = "Cost", action = "Index" }
+            );
+
+            routes.MapRoute(
+                name: "DocumentManagement",
+                url: "evrak-islemleri",
+                defaults: new { controller = "Document", action = "Index" }
+            );
+
+            routes.MapRoute(
+                name: "CompanyInformation",
+                url: "firma-bilgileri",
+                defaults: new { controller = "Company", action = "Index" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
