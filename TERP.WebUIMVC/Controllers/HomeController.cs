dc7b7de8795fd1bc12056aa676497b335ca15579
Güney Balcı <guneybalci@gmail.com>
2022-01-25 00:19:32 +0300

    homecontroller added to car status count to header set

--- TERP.WebUIMVC/Controllers/HomeController.cs
+++ TERP.WebUIMVC/Controllers/HomeController.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TERP.Business.Abstract;
+using TERP.Business.Concrete;
+using TERP.WebUIMVC.Models;
 
 namespace TERP.WebUIMVC.Controllers
 {
     public class HomeController : BaseController
     {
 
+        private ICarService _carService;
+
+        public HomeController()
+        {
+            _carService = new CarManager();
+        }
         public ActionResult Index()
         {
-            return View();
+            return View(new CarViewModel() { 
+                CarList = _carService.GetAllWithCarTypeAndPersonal(), 
+            });
         }
     }
 }
